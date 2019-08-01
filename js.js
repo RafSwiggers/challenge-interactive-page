13ad06ebbdd66a1b9ea5f55fc977e17f4dcebbae
RafSwiggers <rafswiggers@gmail.com>
2019-08-01 13:40:11 +0200

    set slides to invisible for start.

--- js.js
+++ js.js
@@ -1,6 +1,10 @@
 const tabs = document.getElementsByClassName("tabs");
+const slides = document.getElementsByTagName('img');
 var next = document.getElementById("volgende");
 var previous = document.getElementById("vorige");
+var images = ["foto1.jpeg", "foto2.jpeg", "foto3.jpeg"];
+slides[0].src = images[0];
+var slideindex = 0;
 for (var i = 0; i < tabs.length; i++) {
     if (i != 0) {
         tabs[i].style.display = "none";
@@ -8,11 +12,4 @@ for (var i = 0; i < tabs.length; i++) {
 
 }
 
-function textclick() {
-    console.log("yasss")
-};
-
-previous.addEventListener("click", textclick);
-
-
-next.addEventListener("click", textclick);
+next.addEventListener('click', ext)
