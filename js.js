9a6119cd6598613606de9336a4b6bb36fa2e93e6
RafSwiggers <rafswiggers@gmail.com>
2019-08-01 11:28:00 +0200

    start script test

--- js.js
+++ js.js
@@ -1,1 +1,4 @@
-var slides = document.getElementsByClassName("slideshow");
+const tabs = document.getElementsByClassName("tabs");
+for (i = 0, i < tabs.length, i++) {
+    console.log("tabssss");
+}
